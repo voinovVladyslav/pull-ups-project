ec13ebdbaf753905b039769408677af216838749
Nikolas <ZaL1Vv2003@gmail.com>
2023-05-14 13:26:00 +0200

    added test allowed methods for login user

--- tests/user/test_user_login.py
+++ tests/user/test_user_login.py
@@ -34,3 +34,35 @@ def test_login_with_missing_email_or_password(db, api_client: APIClient, user):
     }
     response = api_client.post(LOGIN_USER_URL, data=payload)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+@pytest.mark.parametrize(
+    'method_name,response_code',
+    [
+        ('get', status.HTTP_405_METHOD_NOT_ALLOWED),
+        ('put', status.HTTP_405_METHOD_NOT_ALLOWED),
+        ('patch', status.HTTP_405_METHOD_NOT_ALLOWED),
+        ('delete', status.HTTP_405_METHOD_NOT_ALLOWED),
+        ('post', status.HTTP_200_OK),
+    ],
+    ids=[
+        'GET NOT ALLOWED',
+        'PUT NOT ALLOWED',
+        'PATCH NOT ALLOWED',
+        'DELETE NOT ALLOWED',
+        'POST ALLOWED',
+    ]
+)
+def test_allowed_methods_for_login_user(
+        db,
+        api_client: APIClient,
+        user,
+        user_password,
+        method_name,
+        response_code,
+):
+    payload = {
+        'email': user.email,
+        'password': user_password,
+    }
+    response = getattr(api_client, method_name)(LOGIN_USER_URL, data=payload)
+    assert response.status_code == response_code
