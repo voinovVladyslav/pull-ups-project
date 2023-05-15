4d12e195b597710cd39645428e601351bd57a700
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-05-15 18:14:07 +0300

    models for bars and addresses

--- bars/models.py
+++ bars/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Address(models.Model):
+    country = models.CharField(
+        max_length=128,
+    )
+    city = models.CharField(
+        max_length=256,
+    )
+    street = models.CharField(
+        max_length=256
+    )
+    number = models.CharField(
+        max_length=32,
+    )
+    postal_code = models.CharField(
+        max_length=32,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return f'{self.street} {self.number}'
+
+
+class Bars(models.Model):
+    title = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    latitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=8,
+        validators=[
+            MinValueValidator(-90),
+            MaxValueValidator(90),
+        ]
+    )
+    longitude = models.DecimalField(
+        max_digits=11,
+        decimal_places=9,
+        validators=[
+            MinValueValidator(-180),
+            MaxValueValidator(180),
+        ]
+    )
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Pull bar #{self.id}'
