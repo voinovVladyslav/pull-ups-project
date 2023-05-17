7cad38f132d2dbdad423214354c64e8322a3fd97
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-05-17 20:19:48 +0300

    pagination
    set pagination for bars api
    order results by -id

--- bars/views.py
+++ bars/views.py
@@ -1,9 +1,31 @@
 from rest_framework import viewsets, permissions
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 
+from utils.pagination import StandartResultPagination
 from .models import Bars
 from .serializers import BarsSerializer
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'page',
+                OpenApiTypes.INT,
+                description='Page number',
+            ),
+        ]
+    )
+)
 class BarsViewSet(viewsets.ModelViewSet):
     queryset = Bars.objects.all()
     serializer_class = BarsSerializer
+    pagination_class = StandartResultPagination
+
+    def get_queryset(self):
+        return self.queryset.order_by('-id')
