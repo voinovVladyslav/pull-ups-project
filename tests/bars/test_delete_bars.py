760b24c02f6919361ab3e034d6bc5fa82fdb128e
Nikolas <ZaL1Vv2003@gmail.com>
2023-05-18 14:52:43 +0200

    additional tests to delete bars

--- tests/bars/test_delete_bars.py
+++ tests/bars/test_delete_bars.py
@@ -1,19 +1,29 @@
 import pytest
 from rest_framework import status
+from rest_framework.test import APIClient
 from model_bakery.baker import make
 
 from bars.models import Address, Bars
 from tests.fixtures import api_client
-from .fixtures import bars_payload, address_payload
+from .fixtures import bars_payload, address_payload, create_bars
 from .urls import get_bars_detail_url
 
 
-@pytest.mark.xfail
-def test_delete_success():
-    assert False
+def test_delete_success(db, api_client, create_bars):
+    bars = create_bars
+    assert Bars.objects.count() == 1
+
+    url = get_bars_detail_url(bars.id)
+    response = api_client.delete(url, format='json')
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert Bars.objects.count() == 0
 
 
-@pytest.mark.xfail
-def test_delete_nonexisting_bars_fail():
-    # try to delete by id that does not exist
-    assert False
+def test_delete_nonexisting_bars_fail(db, api_client, create_bars):
+    bars = create_bars
+    assert Bars.objects.count() == 1
+
+    url = get_bars_detail_url(bars.id + 1)
+    response = api_client.delete(url, format='json')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert Bars.objects.count() == 1
