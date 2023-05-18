6021cde211c0bc51ae03fbcb6d31445bed6bb36d
Nikolas <ZaL1Vv2003@gmail.com>
2023-05-18 18:03:50 +0200

    fix update and delete tests

--- tests/bars/test_update_bars.py
+++ tests/bars/test_update_bars.py
@@ -5,7 +5,7 @@ from model_bakery.baker import make
 
 from bars.models import Address, Bars
 from tests.fixtures import api_client
-from .fixtures import bars_payload, address_payload, create_bars
+from .fixtures import bars_payload, address_payload
 from .urls import get_bars_detail_url
 
 
@@ -31,9 +31,9 @@ def test_update_bars_with_address_success(
     assert bars.address == address
 
 
-def test_update_bars_with_empty_values_fail(db, api_client, create_bars):
-    bars = create_bars
-
+def test_update_bars_with_empty_values_fail(db, api_client):
+    address = make(Address)
+    bars = make(Bars, address=address)
     assert Bars.objects.count() == 1
 
     url = get_bars_detail_url(bars.id)
@@ -48,9 +48,31 @@ def test_update_bars_with_empty_values_fail(db, api_client, create_bars):
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
-@pytest.mark.xfail
-def test_partial_update_address_only_success(db, api_client, create_bars):
-    assert False
+def test_partial_update_address_only_success(db, api_client):
+    address = make(Address)
+    bars = make(Bars, address=address)
+    assert Bars.objects.count() == 1
+
+    url = get_bars_detail_url(bars.id)
+    payload = {
+        'address': {
+            'country': 'Update country',
+            'city': 'Update city',
+            'street': 'Update streat',
+            'number': 'Update number',
+            'postal_code': 'Update postal_code',
+        }
+    }
+    response = api_client.patch(url, payload, format='json')
+    response.status_code == status.HTTP_200_OK
+
+    bars.refresh_from_db()
+
+    assert bars.address.country == payload['address']['country']
+    assert bars.address.city == payload['address']['city']
+    assert bars.address.street == payload['address']['street']
+    assert bars.address.number == payload['address']['number']
+    assert bars.address.postal_code == payload['address']['postal_code']
 
 
 @pytest.mark.parametrize(
@@ -67,9 +89,10 @@ def test_partial_update_address_only_success(db, api_client, create_bars):
     ]
 )
 def test_partial_update_bars_only_success(
-    db, api_client, create_bars, object_name, value
+    db, api_client, object_name, value
 ):
-    bars = create_bars
+    address = make(Address)
+    bars = make(Bars, address=address)
 
     assert Bars.objects.count() == 1
 
@@ -86,8 +109,9 @@ def test_partial_update_bars_only_success(
     assert getattr(bars, object_name) == payload[object_name]
 
 
-def test_update_with_coordinates_out_of_range_fail(db, api_client, create_bars):
-    bars = create_bars
+def test_update_with_coordinates_out_of_range_fail(db, api_client):
+    address = make(Address)
+    bars = make(Bars, address=address)
 
     assert Bars.objects.count() == 1
 
