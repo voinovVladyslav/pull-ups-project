f9646d0ce9f34948411e62cd1a214d3a9d0e01b2
Nikolas <ZaL1Vv2003@gmail.com>
2023-05-18 15:55:44 +0200

    additional tests to update bars

--- tests/bars/test_update_bars.py
+++ tests/bars/test_update_bars.py
@@ -1,10 +1,11 @@
 import pytest
+from decimal import Decimal
 from rest_framework import status
 from model_bakery.baker import make
 
 from bars.models import Address, Bars
 from tests.fixtures import api_client
-from .fixtures import bars_payload, address_payload
+from .fixtures import bars_payload, address_payload, create_bars
 from .urls import get_bars_detail_url
 
 
@@ -30,21 +31,88 @@ def test_update_bars_with_address_success(
     assert bars.address == address
 
 
-@pytest.mark.xfail
-def test_update_bars_with_empty_values_fail():
-    assert False
+def test_update_bars_with_empty_values_fail(db, api_client, create_bars):
+    bars = create_bars
+
+    assert Bars.objects.count() == 1
+
+    url = get_bars_detail_url(bars.id)
+    payload = {
+        'title': '',
+        'longitude': '',
+        'latitude': '',
+        'address': ''
+    }
+    response = api_client.patch(url, payload, format='json')
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
-@pytest.mark.xfail
-def test_partial_update_address_only_success():
-    assert False
+def test_partial_update_address_only_success(db, api_client, create_bars):
+    bars = create_bars
+
+    assert Bars.objects.count() == 1
+
+    url = get_bars_detail_url(bars.id)
+    payload = {
+        'longitude': Decimal('90.00'),
+        'latitude': Decimal('15.00'),
+    }
+    response = api_client.patch(url, payload, format='json')
+
+    assert response.status_code == status.HTTP_200_OK
+
+    bars.refresh_from_db()
+
+    assert bars.longitude == payload['longitude']
+    assert bars.latitude == payload['latitude']
+
+@pytest.mark.parametrize(
+'object_name, value',
+    [
+        ('title', 'Updated Country', ),
+        ('longitude', Decimal('110.00')),
+        ('latitude', Decimal('60.00')),
+    ],
+    ids=[
+        'title UPDATE',
+        'longitude UPDATE',
+        'latitude UPDATE',
+    ]
+)
+def test_partial_update_bars_only_success(db, api_client, create_bars, object_name, value):
+    bars = create_bars
+
+    assert Bars.objects.count() == 1
+
+    url = get_bars_detail_url(bars.id)
+    payload = {
+        object_name: value
+    }
+    response = api_client.patch(url, payload, format='json')
+
+    assert response.status_code == status.HTTP_200_OK
+
+    bars.refresh_from_db()
+
+    assert getattr(bars, object_name) == payload[object_name]
 
 
-@pytest.mark.xfail
-def test_partial_update_bars_only_success():
-    assert False
+def test_update_with_coordinates_out_of_range_fail(db, api_client, create_bars):
+    bars = create_bars
 
+    assert Bars.objects.count() == 1
 
-@pytest.mark.xfail
-def test_update_with_coordinates_out_of_range_fail():
-    assert False
+    url = get_bars_detail_url(bars.id)
+    payload = {
+        'longitude': Decimal('-190.00'),
+        'latitude': Decimal('95.00'),
+    }
+    response = api_client.patch(url, payload, format='json')
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    bars.refresh_from_db()
+
+    assert bars.longitude != payload['longitude']
+    assert bars.latitude != payload['latitude']
