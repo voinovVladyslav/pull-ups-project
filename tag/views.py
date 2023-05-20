e5888d04007679f7ed6f4322aa122aa22c147d73
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-05-20 16:28:48 +0300

    make tag viewset and configure urls

--- tag/views.py
+++ tag/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 
-# Create your views here.
+from utils.permissions import ReadOnly
+from utils.pagination import StandartResultPagination
+from .models import Tag
+from .serializers import TagSerializer
+
+
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'page',
+                OpenApiTypes.INT,
+                description='Page number',
+            ),
+        ]
+    )
+)
+class TagViewSet(ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [IsAdminUser | ReadOnly]
+    pagination_class = StandartResultPagination
+
+    def get_queryset(self):
+        return self.queryset.order_by('-id')
