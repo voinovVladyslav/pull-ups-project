a126efa8efe258eebfd7fdd6b776cc56ae95a093
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-05-20 16:03:39 +0300

    model for tag

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
