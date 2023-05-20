78d8c8e18b712bf0041388feed9d459e28622333
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-05-20 16:31:14 +0300

    tag admin

--- tag/admin.py
+++ tag/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'created_at',
+    )
