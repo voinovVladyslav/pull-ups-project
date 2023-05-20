0b23af86f97485b036afcddbbc58cf25a795b430
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-05-20 16:51:38 +0300

    make tag name unique

--- tag/models.py
+++ tag/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
