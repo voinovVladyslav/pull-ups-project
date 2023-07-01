db1584ce9d31b2792565ede2db74b0a5db03677f
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-07-01 19:11:45 +0300

    env for allowed hosts and cors origins

--- pull_up_project/settings.py
+++ pull_up_project/settings.py
@@ -31,12 +31,10 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 
-CORS_ALLOWED_ORIGINS = [
-   'http://localhost:9000',
-]
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', [])
 
 
 # Application definition
