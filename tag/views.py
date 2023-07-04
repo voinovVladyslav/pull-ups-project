659d57e3e48cfcbd162cb6670cea544b89f043b6
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-07-04 19:45:54 +0300

    log create/destroy tag

--- tag/views.py
+++ tag/views.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
 from drf_spectacular.utils import (
@@ -11,6 +12,9 @@ from utils.permissions import ReadOnly
 from utils.pagination import StandartResultPagination
 from .models import Tag
 from .serializers import TagSerializer
+
+
+logger = logging.getLogger('db')
 
 
 @extend_schema_view(
@@ -32,3 +36,18 @@ class TagViewSet(ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.order_by('-id')
+
+    def create(self, request, *args, **kwargs):
+        logger.info(
+            'Create tag',
+            extra=dict(type='tags_create', user=request.user)
+        )
+        return super().create(request, *args, **kwargs)
+
+    def destroy(self, request, pk=None, *args, **kwargs):
+        logger.info(
+            'Deleted tag: id=%s',
+            pk,
+            extra=dict(type='tags_delete', user=request.user)
+        )
+        return super().destroy(request, *args, **kwargs)
