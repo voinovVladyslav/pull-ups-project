11754528c9c3262b8bd2fa48168c253b156cf155
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-07-16 20:09:22 +0300

    feat: register urls for favorites

--- tests/bars/urls.py
+++ tests/bars/urls.py
@@ -2,9 +2,9 @@ from django.urls import reverse
 
 
 BARS_LIST_URL = reverse('bars:bars-list')
-FAVORITE_BARS = reverse('bars:favorites')
-ADD_FAVORITE_BARS = reverse('bars:favorites-add')
-REMOVE_FAVORITE_BARS = reverse('bars:favorites-remove')
+FAVORITE_BARS = reverse('bars:bars-favorites')
+ADD_FAVORITE_BARS = reverse('bars:bars-favorites-add')
+REMOVE_FAVORITE_BARS = reverse('bars:bars-favorites-remove')
 
 
 def get_bars_detail_url(id):
