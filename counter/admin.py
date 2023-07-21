62e4f210a23d1040bb29eb5adba0c2a1140937ee
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-07-21 14:34:03 +0300

    feat: admin for pull up counter

--- counter/admin.py
+++ counter/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import PullUpCounter
 
-# Register your models here.
+from admin_auto_filters.filters import AutocompleteFilter
+
+
+class UserFilter(AutocompleteFilter):
+    title = 'User'
+    field_name = 'user'
+
+
+class BarFilter(AutocompleteFilter):
+    title = 'Pull Up Bar'
+    field_name = 'bar'
+
+
+@admin.register(PullUpCounter)
+class PullUpCounterAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'reps',
+        'user',
+        'bar',
+        'created_at',
+    )
+
+    list_filter = (
+        UserFilter,
+        BarFilter,
+    )
