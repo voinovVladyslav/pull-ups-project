821ba6353b39e78d16a77ad5ae352bdd812bd561
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-07-21 14:28:28 +0300

    feat: pullup counter model

--- counter/models.py
+++ counter/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseCounter(models.Model):
+    class Meta:
+        abstract = True
+
+    reps = models.PositiveIntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class PullUpCounter(BaseCounter):
+    user = models.ForeignKey(
+        'user.User', on_delete=models.CASCADE, related_name='pullup_counter'
+    )
+    bar = models.ForeignKey(
+        'bars.Bars', on_delete=models.CASCADE, related_name='pullup_counter'
+    )
