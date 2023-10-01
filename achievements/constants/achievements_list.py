58b562280b3960210820e4b8de21c81de3a24688
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-01 17:13:47 +0300

    constant list of achievements

---
+++ achievements/constants/achievements_list.py
@@ -0,0 +1,87 @@
+ACHIEVEMENTS = [
+    {
+        'id': 'pullup5',
+        'title': 'Pull up rookie',
+        'description': 'Do 5 pull ups',
+    },
+    {
+        'id': 'pullup10',
+        'title': 'Pull up amator',
+        'description': 'Do 10 pull ups',
+    },
+    {
+        'id': 'pullup15',
+        'title': 'Calistenics enjoyer',
+        'description': 'Do 15 pull ups',
+    },
+    {
+        'id': 'pullup20',
+        'title': 'Maybe try doing muscle ups?',
+        'description': 'Do 20 pull ups',
+    },
+    {
+        'id': 'pullup25',
+        'title': 'Now some challenge',
+        'description': 'Do 25 pull ups',
+    },
+    {
+        'id': 'pullup30',
+        'title': 'Long time to see',
+        'description': 'Do 30 pull ups',
+    },
+    {
+        'id': 'pullup35',
+        'title': 'Yeah, I run out of titles',
+        'description': 'Do 35 pull ups',
+    },
+    {
+        'id': 'pullup40',
+        'title': 'Almost end',
+        'description': 'Do 40 pull ups',
+    },
+    {
+        'id': 'pullup45',
+        'title': 'Come on, 5 more',
+        'description': 'Do 45 pull ups',
+    },
+    {
+        'id': 'pullup50',
+        'title': 'David Goggins',
+        'description': 'Do 50 pull ups',
+    },
+    {
+        'id': 'diff3',
+        'title': 'Easy walk',
+        'description': 'Do pull ups in 3 different places',
+    },
+    {
+        'id': 'diff5',
+        'title': 'Traveler',
+        'description': 'Do pull ups in 5 different places',
+    },
+    {
+        'id': 'diff7',
+        'title': 'Where I am?',
+        'description': 'Do pull ups in 7 different places',
+    },
+    {
+        'id': 'diff10',
+        'title': 'Christopher Columbus',
+        'description': 'Do pull ups in 10 different places',
+    },
+    {
+        'id': 'row7',
+        'title': 'No more rest days',
+        'description': 'Do pull ups 7 days in a row',
+    },
+    {
+        'id': 'row16',
+        'title': '2 - 3 weeks',
+        'description': 'Do pull ups 15 days in a row',
+    },
+    {
+        'id': 'row30',
+        'title': 'Finally, inner peace',
+        'description': 'Do pull ups 30 days in a row',
+    },
+]
