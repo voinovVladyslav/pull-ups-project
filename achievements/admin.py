7b6396976a2324cd48afde190b6278b9473fe676
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-01 16:19:28 +0300

    achievements admin model

--- achievements/admin.py
+++ achievements/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from admin_auto_filters.filters import AutocompleteFilter
 
-# Register your models here.
+from .models import Achievement
+
+
+class UserFilter(AutocompleteFilter):
+    field_name = 'user'
+    title = 'User'
+
+
+@admin.register(Achievement)
+class AchievementModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'user',
+        'done',
+        'achieved_at',
+    )
+
+    raw_id_fields = (
+        'user',
+    )
+
+    list_filter = (
+        'done',
+        UserFilter,
+    )
