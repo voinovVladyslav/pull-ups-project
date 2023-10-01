6f00ab84471bfdd029513da4c5b16afaa7a1bb30
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-01 16:15:16 +0300

    achievement model

--- achievements/models.py
+++ achievements/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Achievement(models.Model):
+    title = models.CharField(
+        max_length=255,
+    )
+    description = models.TextField()
+    done = models.BooleanField(
+        default=False
+    )
+    achieved_at = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
+
+    user = models.ForeignKey(
+        'user.User',
+        on_delete=models.CASCADE,
+        related_name='achievements'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.title} ({self.user.email})'
