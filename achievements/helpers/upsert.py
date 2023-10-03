56693c5cf4ec6d16959c07428e575cf5180d7728
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-03 21:37:47 +0300

    ref: pass user to whom upsert achievements
    update command to get all users and pass it directly to upsert function

--- achievements/helpers/upsert.py
+++ achievements/helpers/upsert.py
@@ -8,42 +8,39 @@ from achievements.constants import ACHIEVEMENTS
 logger = logging.getLogger('db')
 
 
-def upsert_achievements(achievements: list = None):
+def upsert_achievements(user: User, achievements: list = None):
     if not achievements:
         achievements = ACHIEVEMENTS
-
-    users = User.objects.all()
-    for user in users:
-        checked_ids = []
-        for achievement in achievements:
-            obj, created = Achievement.objects.get_or_create(
-                title=achievement['title'],
-                description=achievement['description'],
-                user=user,
-            )
-            if created:
-                logger.info(
-                    'Created achievement %s',
-                    achievement['title'],
-                    extra=dict(
-                        type='achievements',
-                        user=user,
-                    )
-                )
-            checked_ids.append(obj.id)
-
-        excess_achievements = Achievement.objects.filter(
-            user=user
-        ).exclude(
-            id__in=checked_ids
+    checked_ids = []
+    for achievement in achievements:
+        obj, created = Achievement.objects.get_or_create(
+            title=achievement['title'],
+            description=achievement['description'],
+            user=user,
         )
-        if excess_achievements:
+        if created:
             logger.info(
-                'Deleting %s extra achievements',
-                excess_achievements.count(),
+                'Created achievement %s',
+                achievement['title'],
                 extra=dict(
                     type='achievements',
                     user=user,
                 )
             )
-            excess_achievements.delete()
+        checked_ids.append(obj.id)
+
+    excess_achievements = Achievement.objects.filter(
+        user=user
+    ).exclude(
+        id__in=checked_ids
+    )
+    if excess_achievements:
+        logger.info(
+            'Deleting %s extra achievements',
+            excess_achievements.count(),
+            extra=dict(
+                type='achievements',
+                user=user,
+            )
+        )
+        excess_achievements.delete()
--- achievements/management/commands/upsert_achievements.py
+++ achievements/management/commands/upsert_achievements.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
 
 from achievements.helpers.upsert import upsert_achievements
+from user.models import User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        upsert_achievements()
+        users = User.objects.all()
+        for user in users:
+            upsert_achievements(user)
