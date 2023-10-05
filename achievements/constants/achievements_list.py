bd6d29df70419006fefd146903555b9ffb0c28eb
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-05 21:02:06 +0300

    feat: new achievements
    fix typo in other achievements

--- achievements/constants/achievements_list.py
+++ achievements/constants/achievements_list.py
@@ -52,22 +52,22 @@ ACHIEVEMENTS = [
     {
         'id': 'diff3',
         'title': 'Easy walk',
-        'description': 'Do pull ups in 3 different places',
+        'description': 'Do pull ups in 3 different places in one day',
     },
     {
         'id': 'diff5',
         'title': 'Traveler',
-        'description': 'Do pull ups in 5 different places',
+        'description': 'Do pull ups in 5 different places in one day',
     },
     {
         'id': 'diff7',
         'title': 'Where I am?',
-        'description': 'Do pull ups in 7 different places',
+        'description': 'Do pull ups in 7 different places in one day',
     },
     {
         'id': 'diff10',
         'title': 'Christopher Columbus',
-        'description': 'Do pull ups in 10 different places',
+        'description': 'Do pull ups in 10 different places in one day',
     },
     {
         'id': 'row7',
@@ -83,5 +83,55 @@ ACHIEVEMENTS = [
         'id': 'row30',
         'title': 'Finally, inner peace',
         'description': 'Do pull ups 30 days in a row',
+    },
+    {
+        'id': 'totaldiff5',
+        'title': 'Know your surroundings',
+        'description': 'Do pull ups in 5 different places total',
+    },
+    {
+        'id': 'totaldiff25',
+        'title': 'Does my area has that much?',
+        'description': 'Do pull ups in 25 different places total',
+    },
+    {
+        'id': 'totaldiff50',
+        'title': 'Nomad',
+        'description': 'Do pull ups in 50 different places total',
+    },
+    {
+        'id': 'totaldiff100',
+        'title': 'Mr. Worldwide',
+        'description': 'Do pull ups in 100 different places total',
+    },
+    {
+        'id': 'totalpullup100',
+        'title': '10 by 10',
+        'description': 'Total of 100 pull ups',
+    },
+    {
+        'id': 'totalpullup500',
+        'title': 'Circus Athlete',
+        'description': 'Total of 500 pull ups',
+    },
+    {
+        'id': 'totalpullup1000',
+        'title': 'The Pull Up Millennium',
+        'description': 'Total of 1 000 pull ups',
+    },
+    {
+        'id': 'totalpullup5000',
+        'title': 'I have a dream',
+        'description': 'Total of 5 000 pull ups',
+    },
+    {
+        'id': 'totalpullup9000',
+        'title': "It's over 9000!",
+        'description': 'Total of 9 000 pull ups',
+    },
+    {
+        'id': 'totalpullup15000',
+        'title': 'To infinity and beyond',
+        'description': 'Total of 15 000 pull ups',
     },
 ]
