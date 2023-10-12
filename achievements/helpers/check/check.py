8f173cc351278e13d211bd941108c703716464e9
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-12 21:30:02 +0300

    feat: totalpullup achievements tests and able to achieve them

--- achievements/helpers/check/check.py
+++ achievements/helpers/check/check.py
@@ -1,6 +1,8 @@
 from user.models import User
 from .pullup import check_pullup_achievements
+from .totalpullup import check_totalpullup_achievements
 
 
 def check_user_achievements(user: User) -> None:
     check_pullup_achievements(user)
+    check_totalpullup_achievements(user)
