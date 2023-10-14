43a3ae77dd989e66863629048499e2b39ae459be
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-14 21:32:07 +0300

    feat: diff achievements

--- achievements/helpers/check/check.py
+++ achievements/helpers/check/check.py
@@ -1,10 +1,12 @@
 from user.models import User
 from .pullup import check_pullup_achievements
 from .totalpullup import check_totalpullup_achievements
+from .diff import check_diff_achievements
 from .totaldiff import check_totaldiff_achievements
 
 
 def check_user_achievements(user: User) -> None:
     check_pullup_achievements(user)
     check_totalpullup_achievements(user)
+    check_diff_achievements(user)
     check_totaldiff_achievements(user)
