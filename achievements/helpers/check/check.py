4fc150684ef0f06d03ae70c44cd2b147c5bf9d34
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-15 21:39:19 +0300

    feat: streak achievement

--- achievements/helpers/check/check.py
+++ achievements/helpers/check/check.py
@@ -3,6 +3,7 @@ from .pullup import check_pullup_achievements
 from .totalpullup import check_totalpullup_achievements
 from .diff import check_diff_achievements
 from .totaldiff import check_totaldiff_achievements
+from .row import check_row_achievements
 
 
 def check_user_achievements(user: User) -> None:
@@ -10,3 +11,4 @@ def check_user_achievements(user: User) -> None:
     check_totalpullup_achievements(user)
     check_diff_achievements(user)
     check_totaldiff_achievements(user)
+    check_row_achievements(user)
