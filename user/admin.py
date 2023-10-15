e5cfe23b4bf47e15a4a1f4472ede82288e0b495a
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-15 22:03:43 +0300

    feat: admin actions to upsert and check achievements

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
 from user.models import User
+from achievements.helpers.check import check_user_achievements
+from achievements.helpers.upsert import upsert_achievements
 
 
 @admin.register(User)
@@ -58,3 +60,25 @@ class UserAdmin(BaseUserAdmin):
             }
         ),
     )
+    actions = (
+        'upsert_achievements',
+        'check_achievements',
+    )
+
+    def upsert_achievements(self, request, queryset):
+        for user in queryset:
+            upsert_achievements(user)
+        self.message_user(
+            request,
+            f'Successfully upsert achievements for {queryset.count()} users',
+            level=messages.SUCCESS,
+        )
+
+    def check_achievements(self, request, queryset):
+        for user in queryset:
+            check_user_achievements(user)
+        self.message_user(
+            request,
+            f'Successfully checked achievements for {queryset.count()} users',
+            level=messages.SUCCESS,
+        )
