6ba4e8d39542aa6c90660c59c531dbe6b0a4383e
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-22 20:38:04 +0300

    test: stats fields

---
+++ tests/user/test_profile_statistics.py
@@ -0,0 +1,21 @@
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+
+from tests.fixtures import api_client, authenticated_client
+from .fixtures import user_email, user_password
+from .urls import STATS_URL
+
+
+def test_stats_fields(db, authenticated_client: APIClient, user_email):
+    response = authenticated_client.get(STATS_URL)
+    assert response.status_code == status.HTTP_200_OK
+    fields = [
+        'total_pullups',
+        'max_pullups',
+        'total_bars_visited',
+        'current_streak',
+    ]
+    for field in fields:
+        assert field in response.json()
--- tests/user/urls.py
+++ tests/user/urls.py
@@ -3,4 +3,5 @@ from django.urls import reverse
 
 CREATE_USER_URL = reverse('user:create')
 LOGIN_USER_URL = reverse('user:token')
+STATS_URL = reverse('user:stats')
 ME_URL = reverse('user:me')
