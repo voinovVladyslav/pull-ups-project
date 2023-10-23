4a407c84cba7c68662490c3eb1c7f0d875fed330
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-23 21:55:59 +0300

    feat: profile statistics

--- tests/user/test_profile_statistics.py
+++ tests/user/test_profile_statistics.py
@@ -2,7 +2,11 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
+from model_bakery.baker import make
 
+from user.models import User
+from bars.models import Bars
+from counter.models import PullUpCounter
 from tests.fixtures import api_client, authenticated_client
 from .fixtures import user_email, user_password, create_user
 from .urls import STATS_URL
@@ -12,10 +16,28 @@ def test_stats_fields(db, authenticated_client: APIClient, user_email):
     response = authenticated_client.get(STATS_URL)
     assert response.status_code == status.HTTP_200_OK
     fields = [
+        'current_streak',
         'total_pullups',
         'max_pullups',
-        'total_bars_visited',
-        'current_streak',
+        'bars_visited',
+        'bars_visited_today',
     ]
     for field in fields:
         assert field in response.json()
+        assert response.json()[field] == 0
+
+
+def test_calculated_stats(db, authenticated_client: APIClient, user_email):
+    user = User.objects.get(email=user_email)
+    bar = make(Bars)
+    counter = make(PullUpCounter, 2, user=user, bar=bar, reps=33)
+    response = authenticated_client.get(STATS_URL)
+
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+
+    assert data['total_pullups'] == 66
+    assert data['max_pullups'] == 33
+    assert data['current_streak'] == 1
+    assert data['bars_visited'] == 1
+    assert data['bars_visited_today'] == 1
--- user/serializers.py
+++ user/serializers.py
@@ -4,6 +4,13 @@ from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import gettext as _
 
 from achievements.helpers.upsert import upsert_achievements
+from achievements.helpers.check.query import (
+    get_total_different_bars_used_today,
+    get_current_pullup_streak,
+    get_max_pullups,
+    get_total_bars,
+    get_total_pullups
+)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -34,7 +41,8 @@ class UserSerializer(serializers.ModelSerializer):
 class UserStatisticsSerializer(serializers.ModelSerializer):
     total_pullups = serializers.SerializerMethodField()
     max_pullups = serializers.SerializerMethodField()
-    total_bars_visited = serializers.SerializerMethodField()
+    bars_visited = serializers.SerializerMethodField()
+    bars_visited_today = serializers.SerializerMethodField()
     current_streak = serializers.SerializerMethodField()
 
     class Meta:
@@ -42,27 +50,32 @@ class UserStatisticsSerializer(serializers.ModelSerializer):
         fields = [
             'total_pullups',
             'max_pullups',
-            'total_bars_visited',
+            'bars_visited',
+            'bars_visited_today',
             'current_streak',
         ]
         read_only_fields = [
             'total_pullups',
             'max_pullups',
-            'total_bars_visited',
+            'bars_visited',
+            'bars_visited_today',
             'current_streak',
         ]
 
     def get_total_pullups(self, obj):
-        return 0
+        return get_total_pullups(obj)
 
     def get_max_pullups(self, obj):
-        return 0
+        return get_max_pullups(obj)
 
-    def get_total_bars_visited(self, obj):
-        return 0
+    def get_bars_visited(self, obj):
+        return get_total_bars(obj)
+
+    def get_bars_visited_today(self, obj):
+        return get_total_different_bars_used_today(obj)
 
     def get_current_streak(self, obj):
-        return 0
+        return get_current_pullup_streak(obj)
 
 
 class TokenSerializer(serializers.Serializer):
