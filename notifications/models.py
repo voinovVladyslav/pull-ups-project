e5f76ba3191d7d90bfa436f18ce4ed107642c69f
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-28 14:31:26 +0300

    feat: notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Notification(models.Model):
+    message = models.TextField()
+    unread = models.BooleanField(
+        default=True
+    )
+    redirect_to = models.CharField(
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    user = models.ForeignKey(
+        'user.User',
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
