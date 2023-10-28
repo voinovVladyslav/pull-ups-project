ac68b44f08aff1533cbc0acf7cac5e6d41af20ca
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-28 14:53:45 +0300

    ref: move bars autocomplete filter to core filters

--- core/filters.py
+++ core/filters.py
@@ -4,3 +4,8 @@ from admin_auto_filters.filters import AutocompleteFilter
 class UserFilter(AutocompleteFilter):
     title = 'User'
     field_name = 'user'
+
+
+class BarFilter(AutocompleteFilter):
+    title = 'Pull Up Bar'
+    field_name = 'bar'
--- counter/admin.py
+++ counter/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from admin_auto_filters.filters import AutocompleteFilter
 
-from core.filters import UserFilter
+from core.filters import UserFilter, BarFilter
 from .models import PullUpCounter
-
-
-class BarFilter(AutocompleteFilter):
-    title = 'Pull Up Bar'
-    field_name = 'bar'
 
 
 @admin.register(PullUpCounter)
