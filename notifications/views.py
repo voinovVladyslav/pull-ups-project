f1c3ea1c09af58f01678980804cf295985fa6716
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-10-28 15:11:19 +0300

    feat: notification viewset

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from utils.pagination import StandartResultPagination
 
-# Create your views here.
+from .models import Notification
+from .serializers import NotificationSerializer
+
+
+class NotificationViewSet(ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated,]
+    pagination_class = StandartResultPagination
