4036daee1eb1112f2052843d288e52a9f2465568
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-11-29 17:00:39 +0200

    feat: factory method for notifications

--- notifications/helpers.py
+++ notifications/helpers.py
@@ -1,5 +1,14 @@
 from achievements.models import Achievement
+from .models import Notification
 
 
 def get_message_for_achievement(achievement: Achievement) -> str:
     return f'You just got achievement "{achievement.title}"'
+
+
+def create_achievement_notification(user, achievement: Achievement) -> Notification:
+    return Notification.objects.create(
+        user=user,
+        message=get_message_for_achievement(achievement),
+        redirect_to='/achievements'
+    )
