a422aaeb4fde1e668970c20bf9f690a6bebd7110
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-17 08:35:16 +0200

    feat: add/remove from favorites

--- training_ground/views.py
+++ training_ground/views.py
@@ -1,7 +1,9 @@
 from decimal import Decimal
 
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
 from drf_spectacular.utils import (
@@ -61,3 +63,33 @@ class TrainingGroundViewSet(viewsets.ModelViewSet):
             return Point((Decimal(longitude), Decimal(latitude)), srid=4326)
         except Exception:
             return
+
+    @action(
+        methods=['POST'],
+        detail=True,
+        url_name='favorites-add',
+        url_path='favorites/add',
+        permission_classes=[IsAuthenticated],
+        serializer_class=None,
+    )
+    def favorite_add(self, request, pk=None, *args, **kwargs):
+        tg = self.get_object()
+        request.user.favorite_training_grounds.add(tg)
+        return Response(
+            {'message': 'Successfully added to favorites'}, status=200
+        )
+
+    @action(
+        methods=['POST'],
+        detail=True,
+        url_name='favorites-remove',
+        url_path='favorites/remove',
+        permission_classes=[IsAuthenticated],
+        serializer_class=None,
+    )
+    def favorite_remove(self, request, pk=None, *args, **kwargs):
+        tg = self.get_object()
+        request.user.favorite_training_grounds.remove(tg)
+        return Response(
+            {'message': 'Successfully removed from favorites'}, status=200
+        )
